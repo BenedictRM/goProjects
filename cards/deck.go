cdb97b7899dc33abfafc2135f9ef2c8437104d09
BenedictRM <mehringr@gmail.com>
2017-11-08 21:22:27 -0700

    adding file io and basic error handling

--- cards/deck.go
+++ cards/deck.go
@@ -1,12 +1,54 @@
 package main
 
-import "fmt"
+import( 
+	"fmt"
+	"strings"
+	"io/ioutil"
+	"os"
+)
 
 //Create a new type of Deck which is a slice of strings
 //think of this as saying this new deck type extends the behaviour of a slice of strings -- but we don't use oop language in Go ;)
 type deck []string
 
-func (d deck) print(){
+//generate a deck
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) saveToFile(filename string) error{
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+func newDeckFromFile(filename string) deck{
+	bs, err := ioutil.ReadFile(filename)
+	if(err != nil){
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	//Type convert to a string
+	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) toString() string{
+	return strings.Join([]string(d), ",")
+}
+
+
+func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
--- cards/main.go
+++ cards/main.go
@@ -1,11 +1,7 @@
 package main
 
 func main() {
-	cards := deck{newCard(), "Ace"}
-	cards = append(cards, "New CARRRD")
+	cards := newDeckFromFile("MyDeckOfCards")
+	// hand, remainingDeck := deal(cards, 5)
 	cards.print()
-}
-
-func newCard() string {
-	return "5 of Diamonds"
 }
